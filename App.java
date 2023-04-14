593c5fe22635d782f41df062dc346c15834b9574
Mauro Sachs Pereira <21200513@facin.portoalegre.pucrsnet.br>
2023-04-14 20:08:29 -0300

    Menu de funcionalidades

--- App.java
+++ App.java
@@ -3,6 +3,22 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(9999);
+        //ESCOLHER FUNCIONARIO ANTES
+        boolean encerrado = false;
+        while (!encerrado) {
+            //SOMENTE FUNCIONARIOS SUPORTE DEVEM TER ACESSO A ALGUMAS FUNCIONALIADADES
+            System.out.println("Escolher Funcionalidade");
+            System.out.println("1: Abrir novo chamado");
+            System.out.println("2: Atualizar status do chamado");
+            System.out.println("3: Mover equipamento");
+            System.out.println("4: Pesquisar equipamento pela descrição");
+            //Fazer as outras
+            System.out.println("5: Encerrar Programa");
+
+
+        }
+
+
+
     }
 }
